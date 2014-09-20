a48841015f4c3b733b2b6940df64214271d60b36
Karen Etheridge <ether@cpan.org>
2014-09-19 18:44:23 -0700

    initial commit
    via [Git::Init] in Dist-Zilla-PluginBundle-Author-ETHER-0.073, github profile.

---
+++ lib/Module/Runtime/Conflicts.pm
@@ -0,0 +1,47 @@
+use strict;
+use warnings;
+package Module::Runtime::Conflicts;
+# ABSTRACT: ...
+# KEYWORDS: ...
+# vim: set ts=8 sw=4 tw=78 et :
+
+
+1;
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Module::Runtime::Conflicts;
+
+    ...
+
+=head1 DESCRIPTION
+
+...
+
+=head1 FUNCTIONS/METHODS
+
+=head2 C<foo>
+
+...
+
+=head1 SUPPORT
+
+=for stopwords irc
+
+Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=Module-Runtime-Conflicts>
+(or L<bug-Module-Runtime-Conflicts@rt.cpan.org|mailto:bug-Module-Runtime-Conflicts@rt.cpan.org>).
+I am also usually active on irc, as 'ether' at C<irc.perl.org>.
+
+=head1 ACKNOWLEDGEMENTS
+
+...
+
+=head1 SEE ALSO
+
+=for :list
+* L<foo>
+
+=cut
---
+++ t/01-basic.t
@@ -0,0 +1,9 @@
+use strict;
+use warnings FATAL => 'all';
+
+use Test::More;
+use if $ENV{AUTHOR_TESTING}, 'Test::Warnings';
+use Module::Runtime::Conflicts;
+
+fail('this test is TODO!');
+done_testing;
