dba08ce64d1d11071e61f0a46fa779cbd54bd658
Karen Etheridge <ether@cpan.org>
2014-09-19 19:47:32 -0700

    initial code, tests, docs

--- lib/Module/Runtime/Conflicts.pm
+++ lib/Module/Runtime/Conflicts.pm
@@ -1,10 +1,22 @@
 use strict;
 use warnings;
 package Module::Runtime::Conflicts;
-# ABSTRACT: ...
-# KEYWORDS: ...
+# ABSTRACT: Provide information on conflicts for Module::Runtime
+# KEYWORDS: conflicts breaks modules prerequisites upgrade
 # vim: set ts=8 sw=4 tw=78 et :
 
+package Module::Runtime::Conflicts;
+
+use Module::Runtime ();
+use Dist::CheckConflicts
+    -dist      => 'Module::Runtime',
+    -conflicts => {
+        eval { Module::Runtime->VERSION('0.14'); 1 } ? ( 'Moose' => '2.1202' ) : (),
+    },
+    -also => [
+        'Package::Stash::Conflicts',
+        'Moose::Conflicts',
+    ];
 
 1;
 __END__
@@ -13,19 +25,18 @@ __END__
 
 =head1 SYNOPSIS
 
-    use Module::Runtime::Conflicts;
+    `moose-outdated`
 
-    ...
+or
+
+    use Module::Runtime::Conflicts;
+    Module::Runtime::Conflicts->check_conflicts;
 
 =head1 DESCRIPTION
 
-...
-
-=head1 FUNCTIONS/METHODS
-
-=head2 C<foo>
-
-...
+This module provides conflicts checking for L<Module::Runtime>, which had a
+recent release that broke some versions of L<Moose>. It is called from
+L<Moose::Conflicts> and C<moose-outdated>.
 
 =head1 SUPPORT
 
@@ -35,13 +46,12 @@ Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Di
 (or L<bug-Module-Runtime-Conflicts@rt.cpan.org|mailto:bug-Module-Runtime-Conflicts@rt.cpan.org>).
 I am also usually active on irc, as 'ether' at C<irc.perl.org>.
 
-=head1 ACKNOWLEDGEMENTS
-
-...
-
 =head1 SEE ALSO
 
 =for :list
-* L<foo>
+* L<Dist::CheckConflicts>
+* L<Moose::Conflicts>
+* L<Dist::Zilla::Plugin::Breaks>
+* L<Dist::Zilla::Plugin::Test::CheckBreaks>
 
 =cut
--- t/01-basic.t
+++ t/01-basic.t
@@ -1,9 +1,14 @@
 use strict;
 use warnings FATAL => 'all';
 
-use Test::More;
-use if $ENV{AUTHOR_TESTING}, 'Test::Warnings';
+# this test resembles one that would be generated with Dist::Zilla::Plugin::Test::CheckBreaks 0.011
+
+use Test::More 0.88;
 use Module::Runtime::Conflicts;
 
-fail('this test is TODO!');
+eval { Module::Runtime::Conflicts->check_conflicts };
+
+diag $@ if $@;
+pass 'conflicts checked via Module::Runtime::Conflicts';
+
 done_testing;
